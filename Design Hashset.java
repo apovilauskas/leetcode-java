0b6272a5e944074085343b96ce327c2bd94072f7
Augustas Povilauskas <augustaspovil@gmail.com>
2026-02-08 09:06:45 +0200

    Create Design Hashset.java

---
+++ Design Hashset.java
@@ -0,0 +1,19 @@
+class MyHashSet {
+    private boolean[] set;
+
+    public MyHashSet() {
+        this.set = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        set[key]=true;
+    }
+    
+    public void remove(int key) {
+        set[key]=false;
+    }
+    
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
